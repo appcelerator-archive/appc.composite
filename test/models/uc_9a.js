c7dea3972856c5d282b9ef45f76f52dc1523f66f
Dawson Toth <dtoth@appcelerator.com>
2015-01-12 16:07:35 -0500

    Update Test Collection Pointers

--- test/models/uc_9a.js
+++ test/models/uc_9a.js
@@ -1,9 +1,9 @@
 module.exports = function(APIBuilder) {
 	return APIBuilder.createModel('uc_9a', {
 		fields: {
-			employees: { type: Array, collection: 'appc.mysql/Composite_UserTable' },
-			teams: { type: Array, collection: 'appc.mssql/trees' },
-			cities: { type: Array, collection: 'appc.mongo/uc_8' },
+			users: { type: Array, collection: 'appc.mysql/Composite_UserTable' },
+			mssql_posts: { type: Array, collection: 'appc.mssql/TEST_Post' },
+			mongo_posts: { type: Array, collection: 'appc.mongo/post' },
 			accounts: { type: Array, collection: 'appc.salesforce/Account' }
 		},
 		connector: 'appc.composite'
