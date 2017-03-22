30c329ae1cc3e6bdba2fbc8a29c202ee032e35eb
pankaj goyal <pagoyal@axway.com>
2017-03-22 14:46:06 +0000

    [RDPP-555] removing changes mistakenly committed before

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,12 +29,12 @@ module.exports = function (grunt) {
 	// Load grunt plugins for modules.
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	// TODO: Uncomment when unit-tests are working
-	grunt.loadNpmTasks('grunt-mocha-istanbul');
+	// grunt.loadNpmTasks('grunt-mocha-istanbul');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 
 	// Register tasks.
 	// TODO: Uncomment when unit-tests are working
-	grunt.registerTask('default', ['jshint', 'mocha_istanbul:coverage', 'clean']);
+	// grunt.registerTask('default', ['jshint', 'mocha_istanbul:coverage', 'clean']);
 	grunt.registerTask('default', ['jshint', 'clean']);
 
 };
--- conf/default.js
+++ conf/default.js
@@ -1,45 +1,47 @@
 module.exports = {
-    connectors: {
-        'appc.salesforce': {
-            url: 'https://test.salesforce.com/',
-            username: 'dtoth@appcelerator.com.appcdev',
-            password: 'mmpResearch4',
-            token: '4qEFyGMb5r0VYZPZBj99Rjukv',
-            modelAutogen: false,
-            schemaRefresh: 3.6e+6 * 24,
-            generateModels: ['Account', 'Contract']
-        },
-        'appc.mssql': {
-            user: 'sa',
-            password: 'Axway123!',
-            server: 'localhost',
-            port: 1433,
-            database: 'master',
-            options: {
-                encrypt: true
-            }
-        },
-        'appc.mysql': {
-            host: '127.0.0.1',
-            database: 'people',
-            user: 'root',
-            password: 'test',
-            port: 3306,
-            generateModelsFromSchema: true,
-            modelAutogen: true
-        },
-        'appc.arrowdb': {
-            requireSessionLogin: false,
-            key: 'eTqafmrgKhhSjreTezjTr00nggsHeDwr',
-            username: 'pagoyal@axway.com',
-            password: '1234567890',
-            generateModelsFromSchema: true,
-            modelAutogen: true
-        },
-        'appc.mongo': {
-            url: 'mongodb://appcconn:Connectors2016@ds035856.mlab.com:35856/appcconn',
-            generateModelsFromSchema: true,
-            modelAutogen: false
-        }
-    }
+	logs: './logs',
+	quiet: false,
+	logLevel: 'error',
+	apikey: 'bYVrelF3EQ8qGaJj/SoSlTyP6IhtA+1Y',
+	admin: {
+		enabled: true,
+		prefix: '/arrow'
+	},
+
+	connectors: {
+		'appc.mysql': {
+			host: 'localhost',
+			database: 'connector',
+			user: 'root',
+			password: '',
+			port: 3306
+		},
+		'appc.arrowdb': {
+			key: '',
+			username: '',
+			password: '',
+			modelAutogen: false
+		},
+		'appc.mongo': {
+			url: 'mongodb://localhost/arrow'
+		},
+		'appc.salesforce': {
+			url: '',
+			username: '',
+			password: '',
+			token: '',
+			modelAutogen: false
+		},
+		'appc.mssql': {
+			user: '',
+			password: '',
+			server: '',
+			port: 1433,
+			database: '',
+
+			options: {
+				encrypt: true
+			}
+		}
+	}
 };
